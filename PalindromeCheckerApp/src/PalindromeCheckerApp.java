cb5ada2f5978ffc0fb0b9d34647b5340bf25366c
vaish20d-del <vaish20d@gmail.com>
2026-03-10 15:18:50 +0530

    UC3

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,16 +1,35 @@
-
-public class PalindromeCheckerApp{
-    public void main(String[] args) {
-        String input = "madam";
-        boolean isPalindrome = true;
-
-        for(int i=0; i< input.length()/2; i++){
-            if(input.charAt(i) != input.charAt(input.length() -1 -i){
-                isPalindrome=false;
-                break;
-            }
+import java.util.Scanner;
+public class PalindromeCheckerApp {
+    public static void reverse_and_check(String word){
+        boolean status = true;
+        String rev="";
+        //reverse
+        for(int i=word.length()-1; i>=0; i--)
+        {
+            rev = rev + word.charAt(i);
         }
-        System.out.println("Is it a palindrome?:" + isPalindrome);
+
+
+        if(word.compareTo(rev) == 0)
+        {
+            System.out.println("Is it a Palindrome?"+status);
+        }
+
+        else
+        {
+            status = false;
+            System.out.println("Is it a Palindrome?"+status);
+
+        }
+
     }
 
+    public static void main(String[] args) {
+
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.print("Input text: ");
+        String name = s1.next();
+        reverse_and_check(name);
+    }
 }
